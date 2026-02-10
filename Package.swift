31ea8cbda8c26ea8fb125277852fc788c64f2905
Joseph Milan K.A <josmilan1103@gmail.com>
2026-02-10 10:41:08 +0530

    Fix: Initial push for IndyCWrapper

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IndyCWrapper",
+    platforms: [
+           .iOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "IndyCWrapper",
+            targets: ["IndyCWrapper"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/decentralised-dataexchange/LibIndy", from: "2025.8.3"),
+        .package(url: "https://github.com/decentralised-dataexchange/libzmq", from: "2025.8.2"),
+    ],
+    
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "IndyCWrapper",
+            dependencies: [
+                "OpenSSL-XM",
+                "LibIndy",
+                .product(name: "libzmq", package: "libzmq"),
+            ],
+            path: "Sources/IndyCWrapper",
+            publicHeadersPath: "Indy"
+        ),
+        .binaryTarget(
+            name: "OpenSSL-XM",
+            path: "Sources/IndyCWrapper/SupportLibraries/OpenSSL-XM.xcframework"
+        ),
+        .testTarget(
+            name: "IndyCWrapperTests",
+            dependencies: ["IndyCWrapper"]
+        ),
+    ]
+)
